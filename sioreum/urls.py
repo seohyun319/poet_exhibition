c05f4702b5d948f2f1f5f61e4a8bafb337ce8b74
seohyun319 <tjguselly0319@gmail.com>
2021-07-08 06:12:40 +0900

    nav and cdn edit

--- sioreum/urls.py
+++ sioreum/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main, name='poetList'),
+    path('', views.main, name='main'),
     path('poet', views.poetList, name='poetList'),
     path('poet/<int:pk>/', views.poetDetail, name='poetDetail'),
 ]
