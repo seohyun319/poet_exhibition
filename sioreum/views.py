d60d56d1f52128f2db6bf0ae77c7e95809b17d7d
seohyun319 <tjguselly0319@gmail.com>
2021-07-12 17:34:46 +0900

    pagination

--- sioreum/views.py
+++ sioreum/views.py
@@ -2,6 +2,7 @@ from typing import FrozenSet
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from .models import Post, VisitForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def main(request):
     return render(request, 'sioreum/main.html', {})
@@ -16,4 +17,14 @@ def poetDetail(request,pk):
 
 def visitList(request):
     visits = VisitForm.objects.all().order_by('created_date')
+    paginator = Paginator(visits, 2)
+    page = request.GET.get('page', 1)
+    try:
+        visits = paginator.get_page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        visits = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        visits = paginator.page(paginator.num_pages)
     return render(request, 'sioreum/visitList.html', {'visits':visits})
