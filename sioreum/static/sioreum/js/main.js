94842e1fd211d022afe3e1de8b3c40548508df6b
seohyun319 <tjguselly0319@gmail.com>
2021-07-22 23:41:22 +0900

    axios

--- sioreum/static/sioreum/js/main.js
+++ sioreum/static/sioreum/js/main.js
@@ -11,6 +11,24 @@ btn.addEventListener('click', () => {
     // textarea.value = "";
 });
 
+// btn.addEventListener('click', async () => {
+//     const textarea = document.querySelector(".visitContent");
+//     const content = textarea.value;
+//     if (!content) {
+//         return;
+//     }
+//     axios.defaults.xsrfCookieName = "csrftoken";
+//     axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+//     const url = '/visitor/write/';
+//     const { data } = await axios.post(url, {
+//         content,
+//     });
+//     modify(data);
+//     textarea.value = "";
+// });
+
+
+
 // const onClick = () => {
 //     const textarea = document.querySelector(`textarea`);
 //     const content = textarea.value;
