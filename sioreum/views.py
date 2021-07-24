30076b72afd28f53ca7e4716dd3685333bf22c96
seohyun319 <tjguselly0319@gmail.com>
2021-07-25 01:53:19 +0900

    trying date...

--- sioreum/views.py
+++ sioreum/views.py
@@ -48,14 +48,21 @@ def visitCreate(request):
         content = req['content']
         name = req['name']
         date = req['date']
+        y=date[2:4]
+        m=date[5:7]
+        d=date[8:10]
+        time=y+'.'+m+'.'+d
         # phone = req['phone']
         if content:
             visitNew = VisitForm(
-                text=content, author=name, created_date=date,
+                text=content, author=name, 
+                created_date=time,
                 # phone=phone 
                 )
             visitNew.save()
-        return JsonResponse({'comment':visitNew.text, 'writer':visitNew.author, 'time':visitNew.created_date })
+        return JsonResponse({'comment':visitNew.text, 'writer':visitNew.author, 'time':visitNew.created_date, 
+        'y':y, 'm':m, 'd':d, 
+        })
 
 # def visitCreate(request):
 #     content = get_param(request, 'content', '')
