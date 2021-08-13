1661be8cf72c90fde6cf9bc7bd3d261b47555ab2
seohyun319 <tjguselly0319@gmail.com>
2021-08-13 14:20:33 +0900

    edit setting

--- config/settings.py
+++ config/settings.py
@@ -34,14 +34,15 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'sioreum',
+
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    
-    'sioreum',
+    'django.contrib.staticfiles', 
 ]
 
 MIDDLEWARE = [
